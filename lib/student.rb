686e95a4a2cf1f62ece3f41b58ca631f794704fa
Steve Albonico <salbonico@gmail.com>
2019-01-22 19:54:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,13 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-binding.pry
+    @twitter = attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin]
+    @github = attributes_hash[:github]
+    @blog = attributes_hash[:blog]
+    @profile_quote = attributes_hash[:profile_quote]
+    @bio = attributes_hash[:bio]
+    @profile_url = attributes_hash[:profile_url]
   end
 
   def self.all
